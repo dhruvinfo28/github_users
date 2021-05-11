f2b5707d73d5ef8a028473f8f1072980179420b7
dhruvinfo28 <dhruv.info28@gmail.com>
2021-05-11 17:51:58 +0530

    Error Handling

--- src/user/user.jsx
+++ src/user/user.jsx
@@ -4,6 +4,8 @@ import './user.css'
 const User = () =>{
     const [users,setUsers] = useState([]);
     const [index,setIndex] = useState(0);
+    const [isError,setIsError] = useState(false);
+
     const url = `https://api.github.com/users`;
     
     const nextIndex = ()=>{
@@ -39,26 +41,38 @@ const User = () =>{
                 setUsers(user);
             })
             .catch(err=>{
-                
+                setIsError(true);
             })
     },[])
 
-    if(users.length==0){
-        return <div>
+    if(users.length==0 && !isError){
+        return <div className="container">
         <h3>
             Loading ...
             </h3>
         </div>;
     }
-    return <div className="container">
-        <img src={users[index].avatar_url} className="children" alt="" />
-        <h3 className="children">{users[index].login}</h3>
-        <p className="children"><a href={users[index].html_url}>{users[index].html_url}</a> </p>
-        <div className="buttons">
-        <button onClick={prevIndex} className="children">&lt;</button>
-        <button onClick={nextIndex} className="children">&gt;</button>
-        </div>
-    </div>;
+    else if(!isError){
+        return <div className="container">
+            <img src={users[index].avatar_url} className="children" alt="" />
+            <h3 className="children">{users[index].login}</h3>
+            <p className="children"><a href={users[index].html_url}>{users[index].html_url}</a> </p>
+            <div className="buttons">
+            <button onClick={prevIndex} className="children">&lt;</button>
+            <button onClick={nextIndex} className="children">&gt;</button>
+            </div>
+        </div>;
+    }
+    else{
+        return <>
+            <div className="container">
+               <h3>
+                    Sorry, try again! Problem in data fetching
+                   </h3>
+            </div>
+        </>;
+
+    }
 }
 
 export default User;
